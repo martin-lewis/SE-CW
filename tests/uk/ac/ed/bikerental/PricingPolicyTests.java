ea2153b24d11418579a2c658b32833670d7e4646
Martin Lewis <martin.lewis4321@gmail.com>
2019-11-13 15:48:11 +0000

    Fix for APricePolicy
    Prior code only calculated the price for a day, new lines multiply this by the length of the hire as given by the argument.

--- tests/uk/ac/ed/bikerental/PricingPolicyTests.java
+++ tests/uk/ac/ed/bikerental/PricingPolicyTests.java
@@ -3,22 +3,38 @@ package uk.ac.ed.bikerental;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Collections;
 
 import org.junit.jupiter.api.*;
 
 
 public class PricingPolicyTests {
     // You can add attributes here
+    
+    private APricingPolicy testPolicy;
+    private ArrayList<Bike> testBikes;
+    private DateRange dates;
+    
 
     @BeforeEach
     void setUp() throws Exception {
+        testPolicy = new APricingPolicy();
+        
+        dates = new DateRange(LocalDate.of(2019,10,1),LocalDate.of(2019,10,5));
+        
         BigDecimal testPrice = new BigDecimal(50);
         BikeType testType = new BikeType(testPrice);
+        testBikes = new ArrayList<Bike>();
+        for (int i = 0; i < 10; i++) {
+            testBikes.add(new Bike(testType));
+        }
     }
     
     // TODO: Write tests for pricing policies
     @Test
     public void test1() {
-        assertEquals(true, true);
+        BigDecmial calcVal = testPolicy.calculatePrice(testBikes, dates)
     }
 }
