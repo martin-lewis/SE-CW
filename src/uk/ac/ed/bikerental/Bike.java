973e81c56b1e30390834e022ba49286950a4639b
Martin Lewis <martin.lewis4321@gmail.com>
2019-11-14 14:14:53 +0000

    Bike JavaDoc

--- src/uk/ac/ed/bikerental/Bike.java
+++ src/uk/ac/ed/bikerental/Bike.java
@@ -1,9 +1,14 @@
 package uk.ac.ed.bikerental;
-
+/**
+ * Class that represents the individual bikes in the system
+ */
 public class Bike {
     
     final BikeType bikeType;
-    
+    /**
+     * Constructor for Bike
+     * @param bikeType Takes the type of bike, this is a final attribute
+     */
     Bike (BikeType bikeType) {
         this.bikeType = bikeType;
     }
