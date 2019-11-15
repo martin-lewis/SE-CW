a36c329b19da4fc4a94638e0f5f69f6702eac588
Martin Lewis <martin.lewis4321@gmail.com>
2019-11-15 16:19:10 +0000

    Some new classes

---
+++ src/uk/ac/ed/bikerental/Booking.java
@@ -0,0 +1,12 @@
+package uk.ac.ed.bikerental;
+
+import java.util.ArrayList;
+
+public class Booking {
+    
+    private Customer customer;
+    private Provider provider;
+    private ArrayList<Bike> bikes;
+    private String state;
+
+}
