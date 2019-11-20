aefce351764046d37ae6d0d6296a304fe135cf1f
AlDacMac <alasdairmacgsign@gmail.com>
2019-11-20 12:53:42 +0000

    did that last one again

--- src/uk/ac/ed/bikerental/Controller.java
+++ src/uk/ac/ed/bikerental/Controller.java
@@ -7,10 +7,21 @@ public class Controller {
     private ArrayList<Provider> providers;
     private DeliveryService deliveryService;
     
+    /**
+     * Registers the return of a booking to its provider
+     * @return A BigDecimal representing the deposit owed in pounds
+     */
     public BigDecimal registerReturn(Booking booking) {
         return booking.providerReturn();
     }
     
+    /**
+     * Registers the return of a booking to a partner of its provider.
+     * Note that input validation need not be done on the partner,
+     *  as this would occur when the partner uses the register
+     *  return feature on the website.
+     * @return A BigDecimal representing the deposit owed in pounds
+     */
     public BigDecimal registerReturnPartner(Booking booking, Provider partner) {
         Location partAddress = partner.getAddress();
         Location provAddress = booking.getProviderAddress();
@@ -19,8 +30,14 @@ public class Controller {
         String email = "placeholder";
         String provEmail = booking.getProviderEmail();
         sendEmail(email, provEmail);
+        return booking.partnerReturn();
     }
     
+    /**
+     * Sends an email
+     * @param email
+     * @param address the email address
+     */
     private void sendEmail(String email, String address) {
         return;
     }
