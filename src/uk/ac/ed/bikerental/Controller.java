73db3169c1c94c3c8c7ac0991696cdd6653dca76
Martin Lewis <martin.lewis4321@gmail.com>
2019-11-20 14:54:34 +0000

    Controller additional info
    Added comments and JavaDoc

--- src/uk/ac/ed/bikerental/Controller.java
+++ src/uk/ac/ed/bikerental/Controller.java
@@ -63,12 +63,16 @@ public class Controller {
         return;
     }
     /**
-     * 
-     * @param address
-     * @param dates
-     * @param types
-     * @param noBikes
-     * @return
+     * This method takes a set of conditions and attempts to find suitable quotes within the providers listed
+     * in the controller. If no quotes can be found within the dates given then it will check 3 days either side of
+     * the initial dates. If still no quotes can be found then it will return null
+     * @param address The address the customer has given
+     * @param dates The dates over which the customer wants to hire
+     * @param types The types of the bikes they want
+     * @param noBikes The number of bikes they want
+     * @return Will return either a list of one or more quotes or null
+     * @see Quote
+     * @see Provider
      */
     public ArrayList<Quote> getQuotes(Location address, DateRange dates, ArrayList<String> types, int noBikes){
         ArrayList<Quote> quotes = new ArrayList<Quote>(); //ArrayList to hold found quotes
@@ -86,21 +90,21 @@ public class Controller {
             availableBikes = provider.getAvailableBikes(noBikes, types, dates); //Gets the bikes the provider has available
             if (availableBikes != null) { //If the bikes is null there are no bikes so if its not null and there are
                 BigDecimal hirePrice = provider.calculateHirePrice(availableBikes, dates); //Calculates hirePrice
-                BigDecimal deposit = provider.calculateDeposit(availableBikes);
-                Quote newQuote = new Quote(availableBikes, provider, dates, hirePrice, deposit);
-                quotes.add(newQuote);
+                BigDecimal deposit = provider.calculateDeposit(availableBikes); //Calculates deposit
+                Quote newQuote = new Quote(availableBikes, provider, dates, hirePrice, deposit); //Creates this new quote
+                quotes.add(newQuote); //Adds the new quote
             }
         }
         
-        if (quotes.size() > 0) {
-            return quotes;
-        } else {
-            for (int i = -3; i < 4; i++) {
-                if (i==0) {
-                    continue;
+        if (quotes.size() > 0) { //If there are some quotes
+            return quotes; //they are returned
+        } else { //otherwise if there are none we look around other dates
+            for (int i = -3; i < 4; i++) { //Runs through -3 to 3 inclusive representing the number of days we look either side of the original
+                if (i==0) {  //Skip a shift of 0 days as we have already looked at it
+                    continue; //Moves to next i value
                 }
-                DateRange newDate = dates.shift(i);
-                
+                DateRange newDate = dates.shift(i); //Generates new date
+                //Runs the code from before on the new dates
                 for (Provider provider : inRangeProviders) {
                     ArrayList<Bike> availableBikes = new ArrayList<Bike>();
                     availableBikes = provider.getAvailableBikes(noBikes, types, newDate);
@@ -112,10 +116,10 @@ public class Controller {
                     }
                 }
             }
-            if (quotes.size() > 0) {
-                return quotes;
-            } else {
-                return null;
+            if (quotes.size() > 0) { //If once we have run the shifted dates we have some quotes
+                return quotes; //they are returned
+            } else { //otherwise we have exhausted all options
+                return null; //null is returned
             }
         }
         
